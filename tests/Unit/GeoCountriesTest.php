5b5b6fd4c5ec5957c6ebd638389d9b59bbc3f4da
yak0d3 <contact.raedyak@gmail.com>
2019-01-06 07:44:49 +0100

    Add 'GeoCountries' trait tests

---
+++ tests/Unit/GeoCountriesTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace LaraCrafts\GeoRoutes\Tests\Unit;
+
+use LaraCrafts\GeoRoutes\Tests\TestCase;
+use LaraCrafts\GeoRoutes\GeoCountries;
+
+class GeoCountriesTest extends TestCase
+{
+    use GeoCountries;
+
+    /**
+     * @test
+     * @medium
+     */
+    public function getCountryNameReturnsCountryNameForValidCountryCode()
+    {
+        $countryName = $this->getCountryName('tn');
+        $this->assertEquals('Tunisia', $countryName);
+    }
+    
+    /**
+     * @test
+     * @medium
+     * @expectedException \InvalidArgumentException
+     */
+    public function getCountryNameThrowsExceptionForInvalidCountryCode()
+    {
+        $this->getCountryName('INVALID');
+    }
+
+    /**
+     * @test
+     * @medium
+     */
+    public function getCountryCodeReturnsCodeForValidCountryName()
+    {
+        $countryCode = $this->getCountryCode('Canada');
+        $this->assertEquals('CA', $countryCode);
+    }
+
+    /**
+     * @test
+     * @medium
+     * @expectedException \InvalidArgumentException
+     */
+    public function getCountryCodeThrowsExceptionForInvalidCountryName()
+    {
+        $this->getCountryCode('INVALID');
+    }
+
+    /**
+     * @test
+     * @small
+     */
+    public function isValidCountryCodeReturnsTrueForValidCountryCode()
+    {
+        $this->assertTrue($this->isValidCountryCode('us'));
+    }
+
+    /**
+     * @test
+     * @small
+     */
+    public function isValidCountryCodeReturnsFalseForInvalidCountryCode()
+    {
+        $this->assertFalse($this->isValidCountryCode('INVALID'));
+    }
+
+    /**
+     * @test
+     * @small
+     */
+    public function isValidCountryNameReturnsTrueForInvalidCountryName()
+    {
+        $this->assertTrue($this->isValidCountryName('United States'));
+    }
+
+    /**
+     * @test
+     * @small
+     */
+    public function isValidCountryNameReturnsFalseForInvalidCountryName()
+    {
+        $this->assertFalse($this->isValidCountryName('INVALID'));
+    }
+}
