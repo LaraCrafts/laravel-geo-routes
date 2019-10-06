40840ee84c3d6b7a99eab482ee0fa347085b2b63
yak0d3 <contact.raedyak@gmail.com>
2019-10-06 10:52:53 +0100

    Import the HasCallback trait into the GeoRoute class

--- src/GeoRoute.php
+++ src/GeoRoute.php
@@ -4,13 +4,14 @@ namespace LaraCrafts\GeoRoutes;
 
 use BadMethodCallException;
 use Illuminate\Routing\Route;
-use Illuminate\Support\Str;
 
 /**
  * @mixin \Illuminate\Routing\Route
  */
 class GeoRoute
 {
+    use Concerns\HasCallback;
+
     /**
      * Rule is applied.
      *
@@ -19,26 +20,11 @@ class GeoRoute
     protected $applied;
 
     /**
-     * The callback to execute if the visitor
-     * is not allowed.
-     *
-     * @var array
-     */
-    protected $callback;
-
-    /**
      * The countries to apply the rule for.
      *
      * @var array
      */
     protected $countries;
-
-    /**
-     * The callbacks' proxies.
-     *
-     * @var array
-     */
-    protected static $proxies;
 
     /**
      * The route.
@@ -86,7 +72,7 @@ class GeoRoute
             return $this->route->$method(...$arguments);
         }
 
-        if (array_key_exists($method, static::$proxies)) {
+        if ($this->callbackExists($method)) {
             return $this->setCallback(static::$proxies[$method], $arguments);
         }
 
@@ -143,77 +129,6 @@ class GeoRoute
     public function deny()
     {
         $this->strategy = 'deny';
-
-        return $this;
-    }
-
-    /**
-     * Load the available proxies.
-     */
-    protected static function loadProxies()
-    {
-        if (static::$proxies !== null) {
-            return;
-        }
-
-        static::$proxies = [];
-        $callbacks = config('geo-routes.routes.callbacks');
-
-        foreach ($callbacks as $key => $callback) {
-            static::$proxies['or' . Str::studly($key)] = $callback;
-        }
-    }
-
-    /**
-     * Return a HTTP 404 error if access is denied.
-     *
-     * @return $this
-     */
-    public function orNotFound()
-    {
-        return $this->setCallback('LaraCrafts\GeoRoutes\Callbacks::notFound', func_get_args());
-    }
-
-    /**
-     * Redirect to given route if access is denied.
-     *
-     * @param string $routeName
-     *
-     * @return $this
-     */
-    public function orRedirectTo(string $routeName)
-    {
-        return $this->setCallback('LaraCrafts\GeoRoutes\Callbacks::redirectTo', func_get_args());
-    }
-
-    /**
-     * Return a HTTP 401 error if access is denied (this is the default behavior).
-     *
-     * @return $this
-     */
-    public function orUnauthorized()
-    {
-        $this->callback = null;
-
-        return $this;
-    }
-
-    /**
-     * Set the callback.
-     *
-     * @param callable $callback
-     * @param array $arguments
-     *
-     * @return $this
-     */
-    protected function setCallback(callable $callback, array $arguments)
-    {
-        if (is_string($callback) && Str::contains($callback, '@')) {
-            $callback = Str::parseCallback($callback, '__invoke');
-            $callback[0] = resolve($callback[0]);
-        }
-
-        $this->callback = [$callback, $arguments];
 
         return $this;
     }
