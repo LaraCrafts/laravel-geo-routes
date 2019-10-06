b844b5bf775b9829ace58c0465bbbdcb9b993724
yak0d3 <contact@raedyak.tn>
2019-10-06 15:44:07 +0100

    Add mock callbacks class

---
+++ tests/Mocks/Callbacks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaraCrafts\GeoRoutes\Tests\Mocks;
+
+class Callbacks
+{
+    public static function foo()
+    {
+        return 'foo';
+    }
+
+    public static function bar()
+    {
+        return 'bar';
+    }
+}
